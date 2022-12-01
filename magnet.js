51f0fe7b44962c26220c62fbf2d7239badadbe47
_ <seiya-git@users.noreply.github.com>
2022-12-01 21:01:35 +0300

    Update magnet.js

--- magnet.js
+++ magnet.js
@@ -1,15 +1,5 @@
-// onload
-function bindReady(handler){var called=false;function ready(){if(called)return;called=true;handler();}
-function tryScroll(){if(called)return;if(!document.body)return;try{document.documentElement.doScroll('left');ready();}
-catch(e){setTimeout(tryScroll,0);}}if(document.addEventListener){document.addEventListener('DOMContentLoaded',function(){ready();},false);}
-else if(document.attachEvent){if(document.documentElement.doScroll&&window==window.top){tryScroll();}
-document.attachEvent('onreadystatechange',function(){if(document.readyState==='complete'){ready();}});}
-if(window.addEventListener)window.addEventListener('load',ready,false);else if(window.attachEvent)
-window.attachEvent('onload',ready);else window.onload=ready;}var readyList=[];function onReady(handler)
-{if(!readyList.length){bindReady(function(){for(var i=0;i<readyList.length;i++){readyList[i]();}});}readyList.push(handler);}
-
-onReady(function(){
-	var thash = getUri('hash')[0] && checkHash(getUri('hash')[1]) ? getUri('hash')[1] : false;
+document.addEventListener("DOMContentLoaded", function(){
+	var thash = new URLSearchParams(location.search).get('hash');
 	if(thash){
 		document.getElementById('hash').value = thash;
 		var urlr = update();
@@ -22,11 +12,6 @@ onReady(function(){
 
 function encUri(text){
 	return encodeURIComponent(text);
-}
-
-function getUri(name) {
-	var uriEl = (RegExp(name + '=' + '(.+?)(&|$)', 'i').exec(location.search));
-	return uriEl ? [true, decodeURIComponent(uriEl[1].split('&')[0])] : [false, ''];
 }
 
 function sel_all(el){
